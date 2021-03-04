00b543ee547cf3ae4c96500735cbb93206a05e3f
Dileep Singh <dileep.k.singh@synapse.com>
2021-03-04 18:59:00 +0530

    update

--- providers.tf
+++ providers.tf
@@ -2,13 +2,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      source = "hashicorp/azurerm"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+
+  version = "=2.46.0"
   features {}
 }
