8ff7741313e0eda6e2c12790efe73bb631cb5cc4
Dileep Singh <dileep.k.singh@synapse.com>
2021-03-05 13:51:06 +0530

    chnage

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    subscription_id      = "2ee6da9b-a647-401e-9408-ab64ec71fb25"
     resource_group_name  = "k8sdem-norg"
     storage_account_name = "newk8sstorage"
     container_name       = "tfstate"
--- providers.tf
+++ providers.tf
@@ -11,8 +11,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  # subscription_id = "2ee6da9b-a647-401e-9408-ab64ec71fb25"
-  # client_id       = "27391fe5-8f5f-4bd7-b891-c28753f5abf2"
-  # client_secret   = "Vx16CeDN4fuv_1k5Zs4vnS37~YG~.EK8L."
-  # tenant_id       = "0dc069a3-cd7d-49f7-bd71-4ebf572249f3"
+  subscription_id = "2ee6da9b-a647-401e-9408-ab64ec71fb25"
+  client_id       = "27391fe5-8f5f-4bd7-b891-c28753f5abf2"
+  client_secret   = "Vx16CeDN4fuv_1k5Zs4vnS37~YG~.EK8L."
+  tenant_id       = "0dc069a3-cd7d-49f7-bd71-4ebf572249f3"
 }
