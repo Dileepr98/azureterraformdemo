cedc101fe7e4ee284af1647367b508b56ff96944
Dileep Singh <dileep.k.singh@synapse.com>
2021-03-05 14:29:05 +0530

    Updaet

--- main.tf
+++ main.tf
@@ -3,13 +3,6 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr_test.id
-  role_definition_name             = "Contributor"
-  principal_id                     = azurerm_kubernetes_cluster.aks_test_cluster.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
-
 resource "azurerm_container_registry" "acr_test" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
